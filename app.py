c33fc13dfaa73ecbf6bc5434a23df4a92a0519e5
Rishav Kumar <rishavkumar1951@gmail.com>
2025-04-04 16:28:37 +0530

    add test logic

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ def add(a, b):
     return a+b
 
 
-def sub(a, b): return a-b
+def sub(a, b):
+    return a-b
 
 
-result1 = add(4, 5)
-result2 = sub(4, 1)
+def mul(a, b):
+    return a*b
 
 
-print(result1, result2)
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
+
+    print(result1, result2)
